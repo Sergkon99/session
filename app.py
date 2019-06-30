6342479e46a4b5db88eca550bd8e96de5bb9792b
Sergkon99 <37593436+Sergkon99@users.noreply.github.com>
2019-06-30 15:55:41 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, session
+from functools import wraps
+
+app = Flask(__name__)
+app.secret_key = 'qwerty1234'
+
+
+def check_logged_in(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'is_logged' in session:
+            return func(*args, **kwargs)
+        return 'You are NOT logged in.'
+    return wrapper
+
+
+@app.route('/')
+def index():
+    return 'Index page'
+
+
+@app.route('/login/<user>')
+def login(user: str) -> str:
+    session['is_logged'] = True
+    session['user_name'] = user
+    return 'Hi, {0}. You are successful logged in'.format(session['user_name'])
+
+
+@app.route('/logout')
+def logout():
+    session.pop('is_logged')
+    tmp = session.pop('user_name')
+    return 'Bye, {0}. You are successful logged out'.format(tmp)
+
+
+@app.route('/profile')
+@check_logged_in
+def get_profile():
+    return 'Hello, {}'.format(session['user_name'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
